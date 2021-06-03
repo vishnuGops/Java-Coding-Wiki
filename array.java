f9c506f711a31b8d72d05874ac285954b1cd2808
Vishnu Gopal <vishvargops@gmail.com>
2021-06-02 20:24:51 -0500

    commit

--- array.java
+++ array.java
@@ -8,21 +8,65 @@ public class array {
         int n = 5;
 
         ArrayList<Integer> arr = new ArrayList<Integer>(n);
-        System.out.println("Original array:");
+
         for (int i = 0; i < n; i++) {
             arr.add(i);
         }
 
-        System.out.println(arr);
+        System.out.println("Original array:" + arr);
 
         arr.remove(3);
-        System.out.println("Array after removing 3:");
-        System.out.println(arr);
+
+        System.out.println("Array after removing 3:" + arr);
 
         System.out.println("Each element of Array:");
         for (int i = 0; i < arr.size(); i++) {
             System.out.print(arr.get(i) + " ");
         }
+        System.out.println();
+
+        arr.add(1, 10);
+        System.out.println("Adding element 10 in the 2nd index of the array:" + arr);
+
+        ArrayList<String> str = new ArrayList<String>();
+        str.add("Geeks");
+        str.add("Geeks");
+
+        System.out.println(str);
+        str.add(1, "for");
+        System.out.println(str);
+
+        System.out.println("\nREMOVAL\n");
+
+        str.remove(1); // removing using index
+        System.out.println("Removing the element at first index:" + str);
+
+        str.remove("Geeks"); // removing using object
+        System.out.println("Removing the object: " + str);
+
+        System.out.println("\nITERATING\n");
+
+        str.add("Money");
+        str.add("Kids");
+        str.add("Location");
+        // using for loop
+        for (int i = 0; i < str.size(); i++) {
+            System.out.print(str.get(i) + " ");
+
+        }
+        System.out.println();
+        // using for each loop
+        for (String s : str) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        System.out.println("To check if Money exists in the list: " + str.contains("Money"));
+
+        System.out.println("\nReplace Kids with Monkey");
+        String rep = str.set(2, "Monkey");
+
+        System.out.println("Replaced Array: " + str);
+        System.out.println("Replaced element = " + rep);
 
     }
 
---
+++ removeDuplicatesList.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class removeDuplicatesList {
+    // this is using another array list
+    public static <Integer> ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
+        ArrayList<Integer> newlist = new ArrayList<Integer>();
+
+        for (Integer i : list) {
+            if (!newlist.contains(i)) {
+                newlist.add(i);
+            }
+
+        }
+        return newlist;
+    }
+
+    // we can also make use of LinkedHashSet which hold unique values and no
+    // duplicates
+    // is also faster and more efficient
+
+    public static <Integer> ArrayList removeDups(ArrayList<Integer> list) {
+
+        Set<Integer> set = new LinkedHashSet<Integer>();
+
+        set.addAll(list);
+
+        list.clear();
+
+        list.addAll(set);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
+
+        System.out.println("Original Array: " + list);
+
+        ArrayList<Integer> newList = removeDuplicates(list);
+
+        System.out.println("Array with no Duplicates using another array: " + newList);
+
+        ArrayList<Integer> setlist = removeDups(list);
+        System.out.println("Array with no duplicates using set: " + setlist);
+
+        System.out.println("Using Java8 Stream: ");
+
+        List<Integer> streamList = list.stream().distinct().collect(Collectors.toList());
+
+        System.out.println(streamList);
+
+    }
+
+}
